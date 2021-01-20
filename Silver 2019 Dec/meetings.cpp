c51d6288adb9b6740dcad987eaa5bf1088b2273a
ishaanjav <ishaanjav@gmail.com>
2021-01-20 09:42:35 -0600

    Silver Dec 2019 Solutions

---
+++ Silver 2019 Dec/meetings.cpp
@@ -0,0 +1,52 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fi first
+#define se second
+#define all(v) v.begin(), v.end()
+#include <vector>
+#include <algorithm>
+
+int main() {
+    ifstream cin("meetings.in");
+    ofstream fout("meetings.out");
+
+    ll n, l;
+    cin >> n >> l;
+    ll totalW = 0;
+    vector<pair<int, pll> > ar;  // (x, (w, 1 if right))
+    vector<ll> rT, lT;
+    for (int i = 0; i < n; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        totalW += a;
+        ar.pb(mp(b, mp(a, c)));
+        if (c == -1) lT.pb(b);
+        else rT.pb(l - b);
+    }
+    sort(all(ar)); sort(all(rT)); sort(all(lT));
+    vector<pll> times;
+    int j = lT.size(), k = rT.size() - 1;
+    for (int i = 0; i < n; i++) 
+        {if (i < j) times.pb(mp(lT[i], ar[i].se.fi));
+        else times.pb(mp(rT[k--], ar[i].se.fi));}
+
+    sort(all(times));
+    ll weight, t = 0;
+    for (auto p : times) {
+        weight += p.se;
+        t = p.fi;
+        if (weight * 2 >= totalW) break;
+    }
+    int intersections = 0;
+    for (auto p : ar) 
+        if (p.se.se == 1) intersections += lower_bound(all(lT), p.fi) - upper_bound(all(lT), p.fi + 2 * t);
+    fout << -intersections << endl;
+    return 0;
+}
---
+++ Silver 2019 Dec/milkvisits.cpp
@@ -0,0 +1,79 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ins insert
+#define mp make_pair
+#define pii pair<int, int>
+#define pil pair<int, ll>
+#define pll pair<ll, ll>
+#define pib pair<int, bool>
+#define SET(a, c) memset(a, c, sizeof(a))
+#define MOD 1000000007
+#define Endl "\n"
+#define endl "\n"
+#define fi first
+#define se second
+#define rs resize
+#define len(a) (sizeof(a)/sizeof(a[0])
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define FOUND(u, val) u.find(val) != u.end()
+#define max_self(a, b) a = max(a, b);
+#define min_self(a, b) a = min(a, b);
+#define deb cout << "ASDFASDF\n"
+#define read(ar) \
+    for (auto& x : ar) cin >> x;
+#define each(ar) for (auto i : ar)
+#define eachv(ar, i) for (auto i : ar)
+
+#include <string>
+#include <vector>
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+//#include <algorithm>
+//#include <set>
+//#include <map>
+//#include <unordered_set>
+//#include <unordered_map>
+//#include <cmath>
+//#include <cstring>
+//#include <sstream>
+//#include <stack>
+//#include <queue>
+int n, m;
+string pref;
+vvi adj;
+int id[1000000]; int c =0;
+void dfs(int v, int p){
+    id[v] = c;
+    for(int u : adj[v]) if(pref[u] == pref[v] && !id[u]) dfs(u, v);
+}
+int main() {
+    ifstream cin("milkvisits.in");
+    ofstream fout("milkvisits.out");
+
+    cin >> n >> m >> pref;
+    adj.resize(n + 1);
+    pref = " " + pref;
+    for(int i =0 ; i < n -1; i++){
+        int a, b; cin >> a >> b;
+        adj[a].pb(b); adj[b].pb(a);
+    }
+    for(int i = 1; i <= n; i++){
+        if(id[i]) continue;
+        c++;
+        dfs(i, 0);
+    }
+
+    for(int i = 0; i <m ; i++){
+        int a, b; char c; cin >> a >> b >> c;
+        fout << (id[a] != id[b] || pref[a] == c);
+    }
+
+    return 0;
+}
