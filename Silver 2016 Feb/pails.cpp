99500cd6db5b149aa26ec5fa5d180ffb1bf0c691
ishaanjav <ishaanjav@gmail.com>
2021-02-20 13:09:10 -0600

    Additional Solutions

--- Silver 2016 Feb/pails.cpp
+++ Silver 2016 Feb/pails.cpp
@@ -9,80 +9,37 @@
 using namespace std;
 
 #define ll long long
-#define pb push_back
-#define ins insert
-#define mp make_pair
-#define pii pair<int, int>
-#define pil pair<int, ll>
-#define SET(a, c) memset(a, c, sizeof(a))
-#define MOD 1000000007
-#define enld endl
-#define endl "\n"
-#define fi first
-#define se second
-#define rs resize
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define FOUND(u, val) u.find(val) != u.end()
-#define max_self(a, b) a = max(a, b);
 #define min_self(a, b) a = min(a, b);
-
-#include <string>
-#include <vector>
-typedef vector<int> vi;
-typedef vector<vector<int> > vvi;
-typedef vector<ll> vl;
-typedef vector<bool> vb;
-//#include <algorithm>
-//#include <set>
-//#include <map>
-//#include <unordered_set>
-//#include <unordered_map>
-//#include <cmath>
-#include <cstring>
-//#include <sstream>
-//#include <stack>
-//#include <queue>
-
-int moves[101][101];
-bool canGet[1000];
 int x, y, k, m;
-
+bool dp[101][101][101], done[101][101];
+void recurse(int p = 0, int a = 0, int b = 0){
+    if(dp[a][b][p]) return;
+    dp[a][b][p] = 1;
+    if(p ++< k){
+        recurse(p, x, b); // fill a to the top
+        recurse(p, a, y); // fill b to the top
+        // empty
+        recurse(p, 0, b);
+        recurse(p, a, 0);
+        // pour
+        int toB = min(y - b, a);
+        recurse(p, a - toB, b + toB);
+        int toA = min(x - a, b);
+        recurse(p, a + toA, b - toA);
+    }
+}
 int main() {
     ifstream cin("pails.in");
     ofstream fout("pails.out");
-
+    
     cin >> x >> y >> k >> m;
-
-    bool can[x + 1][y + 1];
-    SET(can, false);
-
-    can[0][0] = true;
-    for (int o = 1; o <= k; o++) {
-        bool next[x + 1][y + 1];
-        SET(next, false);
-        for (int i = 0; i < x + 1; i++) {
-            for (int j = 0; j < y + 1; j++) {
-                if (!can[i][j]) continue;
-                next[i][j] = 1;
-                next[0][j] = 1;
-                next[x][j] = 1;
-                next[i][y] = 1;
-                int moveRight = min(i, y - j);
-                int moveLeft = min(x - i, j);
-                next[i - moveRight][j + moveRight] = 1;
-                next[i + moveLeft][j - moveLeft] = 1;
-            }
-        }
-        memcpy(can, next, sizeof(can));
-    }
-    int ret = x + y;
-    for (int i = 0; i <= x; i++) {
-        for (int j = 0; j <= y; j++) {
-            if (!can[i][j]) continue;
-            ret = min(ret, abs(i + j - m));
-        }
-    }
-    cout << ret << endl;
-    fout << ret << endl;
+    recurse();
+    ll closest = m;
+    for(int p = 0; p <= k; p ++)
+        for(int i = 0; i <= x; i++)
+            for(int j = 0; j <= y; j++)
+                if(dp[i][j][p])
+                    min_self(closest, (ll) abs(m - i - j));
+    fout << closest << endl;
+    return 0;
 }
---
+++ Silver 2016 Feb/pails2.cpp
@@ -0,0 +1,88 @@
+/*
+ * Created by ishaanjav
+ * github.com/ishaanjav
+ * USACO Solutions: https://github.com/ishaanjav/USACO-Solutions
+ */
+
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ins insert
+#define mp make_pair
+#define pii pair<int, int>
+#define pil pair<int, ll>
+#define SET(a, c) memset(a, c, sizeof(a))
+#define MOD 1000000007
+#define enld endl
+#define endl "\n"
+#define fi first
+#define se second
+#define rs resize
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define FOUND(u, val) u.find(val) != u.end()
+#define max_self(a, b) a = max(a, b);
+#define min_self(a, b) a = min(a, b);
+
+#include <string>
+#include <vector>
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<ll> vl;
+typedef vector<bool> vb;
+//#include <algorithm>
+//#include <set>
+//#include <map>
+//#include <unordered_set>
+//#include <unordered_map>
+//#include <cmath>
+#include <cstring>
+//#include <sstream>
+//#include <stack>
+//#include <queue>
+
+int moves[101][101];
+bool canGet[1000];
+int x, y, k, m;
+
+int main() {
+    ifstream cin("pails.in");
+    ofstream fout("pails.out");
+
+    cin >> x >> y >> k >> m;
+
+    bool can[x + 1][y + 1];
+    SET(can, false);
+
+    can[0][0] = true;
+    for (int o = 1; o <= k; o++) {
+        bool next[x + 1][y + 1];
+        SET(next, false);
+        for (int i = 0; i < x + 1; i++) {
+            for (int j = 0; j < y + 1; j++) {
+                if (!can[i][j]) continue;
+                next[i][j] = 1;
+                next[0][j] = 1;
+                next[x][j] = 1;
+                next[i][y] = 1;
+                int moveRight = min(i, y - j);
+                int moveLeft = min(x - i, j);
+                next[i - moveRight][j + moveRight] = 1;
+                next[i + moveLeft][j - moveLeft] = 1;
+            }
+        }
+        memcpy(can, next, sizeof(can));
+    }
+    int ret = x + y;
+    for (int i = 0; i <= x; i++) {
+        for (int j = 0; j <= y; j++) {
+            if (!can[i][j]) continue;
+            ret = min(ret, abs(i + j - m));
+        }
+    }
+    cout << ret << endl;
+    fout << ret << endl;
+}
